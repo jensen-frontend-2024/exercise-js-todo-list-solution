353de55191a2537fa8f5ec120316c3ad65be8e0d
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2024-10-30 10:30:44 +0100

    add done feature to a todo

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const todoInput = document.querySelector(".todo-input");
 // ########## Event Listeners ########## //
 
 form.addEventListener("submit", handleOnSubmit);
+todoList.addEventListener("click", handleOnClick);
 
 // ########## Functions ########## //
 
+/**
+ * The handler to the submit event.
+ * @param {*} event
+ */
 function handleOnSubmit(event) {
   event.preventDefault();
 
@@ -27,13 +32,42 @@ function handleOnSubmit(event) {
 function createTodoElement(newTodo) {
   const newTodoElementString = /*html*/ `
         <article class="todo">
-            <p>${newTodo}</p>
+            <input class="todo-input" type="checkbox">
+            <label class="todo-label">${newTodo}</label>
         </article>
      `;
 
   return newTodoElementString;
 }
 
+/**
+ * Will add the provided todoElementString to the DOM, specifically to the todoListElement.
+ * @param {*} todoElementString
+ */
 function addNewTodoToTodoList(todoElementString) {
   todoList.insertAdjacentHTML("beforeend", todoElementString);
+}
+
+function handleOnClick(event) {
+  const target = event.target;
+  let article;
+
+  if (target.tagName !== "ARTICLE") {
+    article = target.parentElement;
+  } else {
+    article = target;
+  }
+
+  const checkbox = article.querySelector(".todo-input");
+  const label = article.querySelector(".todo-label");
+
+  if (target.tagName !== "INPUT") {
+    checkbox.checked = !checkbox.checked;
+  }
+
+  if (checkbox.checked) {
+    label.classList.add("done");
+  } else {
+    label.classList.remove("done");
+  }
 }
